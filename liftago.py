fc8441839acbd9216dc416c093583d6182fa8ed4
Lukas Benka <lukasbenka@Lukass-MacBook-Pro.local>
2020-02-20 12:39:46 +0100

    finished

--- liftago.py
+++ liftago.py
@@ -1,80 +1,69 @@
+import os.path
+import pickle
 from datetime import datetime, timedelta
 
-import httplib2
-import os
+from google.auth.transport.requests import Request
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
 
-from apiclient import discovery
-from oauth2client import client
-from oauth2client import tools
-from oauth2client.file import Storage
-
-try:
-    import argparse
-
-    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
-except ImportError:
-    flags = None
-
-# If modifying these scopes, delete your previously saved credentials
-# at ~/.credentials/gmail-python-quickstart.json
-SCOPES = 'https://www.googleapis.com/auth/gmail.readonly'
-CLIENT_SECRET_FILE = 'client_secret.json'
-APPLICATION_NAME = 'Gmail API Python Quickstart'
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = ["https://www.googleapis.com/auth/gmail.readonly"]
 
 
-def get_credentials():
-    """Gets valid user credentials from storage.
-
-    If nothing has been stored, or if the stored credentials are invalid,
-    the OAuth2 flow is completed to obtain the new credentials.
-
-    Returns:
-        Credentials, the obtained credential.
+def get_gmail_client():
+    """Shows basic usage of the Gmail API.
+    Lists the user's Gmail labels.
     """
-    home_dir = os.path.expanduser('~')
-    credential_dir = os.path.join(home_dir, '.credentials')
-    if not os.path.exists(credential_dir):
-        os.makedirs(credential_dir)
-    credential_path = os.path.join(credential_dir, 'client_id.json')
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists("token.pickle"):
+        with open("token.pickle", "rb") as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open("token.pickle", "wb") as token:
+            pickle.dump(creds, token)
 
-    store = Storage(credential_path)
-    credentials = store.get()
-    if not credentials or credentials.invalid:
-        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
-        flow.user_agent = APPLICATION_NAME
-        if flags:
-            credentials = tools.run_flow(flow, store, flags)
-        else:  # Needed only for compatibility with Python 2.6
-            credentials = tools.run(flow, store)
-        print('Storing credentials to ' + credential_path)
-    return credentials
+    service = build("gmail", "v1", credentials=creds)
+    return service
 
 
 def main():
-    credentials = get_credentials()
-    http = credentials.authorize(httplib2.Http())
-    gmail = discovery.build('gmail', 'v1', http=http)
+    service = get_gmail_client()
 
     first = datetime.now().date().replace(day=1)
-    last = first + timedelta(days=31)
-    first = first.strftime('%Y/%m/%d')
-    last = last.strftime('%Y/%m/%d')
-    query = 'ride with liftago after:{0} before:{1}'.format(first, last)
+    last = first + timedelta(days=31)  # no need to be super precise
 
-    results = gmail.users().messages().list(userId='me',
-                                            labelIds=None,
-                                            pageToken=None,
-                                            q=query,
-                                            maxResults=25,
-                                            includeSpamTrash=False).execute()
+    results = (
+        service.users()
+        .messages()
+        .list(
+            userId="me",
+            labelIds=None,
+            pageToken=None,
+            q=f"ride with liftago after:{first:%Y/%m/%d} before:{last:%Y/%m/%d}",
+            maxResults=25,
+            includeSpamTrash=False,
+        )
+        .execute()
+    )
+
     wanted_data = []
-    for msg in results.get('messages', []):
-        msg_object = gmail.users().messages().get(userId='me', id=msg.get('id')).execute()
+    for msg in results.get("messages", []):
+        msg_object = service.users().messages().get(userId="me", id=msg.get("id")).execute()
 
-        wanted_data.append(float(msg_object.get('snippet').split()[0][3:]))
+        wanted_data.append(float(msg_object.get("snippet").split()[0][3:]))
 
-    print('For this month it is: {} CZK'.format(sum(wanted_data)))
+    print(f"For this month it is: {sum(wanted_data)} CZK")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
